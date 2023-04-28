8fbb1185395f496fe18bb80b389ae8f4e6035dcc
Kellms <96078965+Kellms@users.noreply.github.com>
2023-04-28 10:46:19 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -13,7 +13,20 @@ package q1
 //A função deve retornar um valor booleano, indicando se é possível ou não dividir a melancia da forma desejada. Se o peso
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
 
+
 func DivideWatermelon(weight int) (bool, error) {
-	// Seu código aqui
+
+	fmt.Println("Informe o Peso da melancia")
+	fmt.Scanln(&weight)
+
+	if weight <= 0 {
+		err := errors.New("peso inválido")
+		fmt.Println(err)
+	}
+	if weight%2 == 0 {
+		fmt.Println("É possível:")
+	} else {
+		fmt.Println("Não é possível:")
+	}
 	return false, nil
 }
